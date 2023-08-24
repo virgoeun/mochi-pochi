b3cd84376c93d54a99269671f9f7a0ab822af7f5
Alice <johndoe@example.com>
2023-08-24 14:05:43 +0200

    final draft

--- Js/game.js
+++ Js/game.js
@@ -1,33 +1,32 @@
 class Game {
-    constructor(setmole) {
-      this.startScreen = document.querySelector("#start-screen"); //first HTML element
-      this.gameScreen = document.querySelector("#game-screen");
-      this.gameEndScreen = document.querySelector("#game-end");
+  constructor(setmole) {
+    this.startScreen = document.querySelector("#start-screen"); //first HTML element
+    this.gameScreen = document.querySelector("#game-screen");
+    this.gameEndScreen = document.querySelector("#game-end");
 
-      this.gameIsOver = false;
-      this.score = 0;
-      this.miss = 0;
-      this.timeLeft = 60;
-      this.squareItem = document.querySelectorAll(".square");
-    }
+    this.gameIsOver = false;
+    this.score = 0;
+    this.miss = 0;
+    this.timeLeft = 60;
+    this.squareItem = document.querySelectorAll(".square");
+  }
+
+
   
-
+  start() {
+    this.gameScreen.style.width = `${this.width}px`;
+    this.gameScreen.style.height = `${this.height}px`;
+    this.startScreen.style.display = "none"; // hide the start screen
+    this.gameScreen.style.display = "block";
+  }
     
-    start() {
-      this.gameScreen.style.width = `${this.width}px`;
-      this.gameScreen.style.height = `${this.height}px`;
-      this.startScreen.style.display = "none"; // hide the start screen
-      this.gameScreen.style.display = "block";
-    }
-      
-   
 
-    endGame() 
-    { 
-      this.gameIsOver = true;
-      this.gameScreen.style.display = "none"; // hide the game screen
-      this.gameEndScreen.style.display = "block";
-    } 
+  endGame() 
+  { 
+    this.gameIsOver = true;
+    this.gameScreen.style.display = "none"; // hide the game screen
+    this.gameEndScreen.style.display = "block";
+  } 
 
-    
+  
 }
